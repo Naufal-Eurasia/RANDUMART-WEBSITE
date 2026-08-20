313a370e04a50368c2573aa3c6c2ac8807d0a553
inoru <inoru375@gmail.com>
2026-08-20 16:04:48 +0700

    tambah tabel bundle dan bundleItem

---
+++ prisma/migrations/20260820061653_add_bundle_tables/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Bundle" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Bundle_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BundleItem" (
+    "id" TEXT NOT NULL,
+    "bundleId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+
+    CONSTRAINT "BundleItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "BundleItem_bundleId_idx" ON "BundleItem"("bundleId");
+
+-- CreateIndex
+CREATE INDEX "BundleItem_productId_idx" ON "BundleItem"("productId");
+
+-- AddForeignKey
+ALTER TABLE "BundleItem" ADD CONSTRAINT "BundleItem_bundleId_fkey" FOREIGN KEY ("bundleId") REFERENCES "Bundle"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BundleItem" ADD CONSTRAINT "BundleItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
